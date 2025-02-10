e80ec66d53ebff1919ecf0390bc4040f185a8bb6
ymj07168 <ymj07168@gmail.com>
2025-02-10 16:57:40 +0900

    tab practice #2

--- 13.Integration/tabbox/frontend/src/TabList.js
+++ 13.Integration/tabbox/frontend/src/TabList.js
@@ -4,7 +4,7 @@ import TabItem from './TabItem'
 function TabList({data}) {
   return (
     <ul>
-        {data.map((item) => <TabItem no={item.no} name={item.name} active={item.active} contents={item.contents}/>)}
+        {data.map((item) => <TabItem no={item.no} key={item.no} name={item.name} active={item.active} contents={item.contents}/>).reverse()}
     </ul>
   )
 }
