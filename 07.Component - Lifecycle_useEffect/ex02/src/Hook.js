9285ce52086885775572113e721b5abefc8b4a7f
ymj07168 <ymj07168@gmail.com>
2025-02-12 16:53:21 +0900

    ex04

--- 07.Component - Lifecycle_useEffect/ex02/src/Hook.js
+++ 07.Component - Lifecycle_useEffect/ex02/src/Hook.js
@@ -7,21 +7,38 @@ export default function Hook({ color }) {
     /**
      *   [1] Alternative 01: getDerivedStateFromProps
      */
-
+    if (boxColor !== color){
+        setBoxColor(color);
+    }
 
     /**
      *   [2-1] After Rendering 함수 - Alternative 02: componentDidUpdate
      */
+    useEffect(() => {
+        console.log('After Rendering');
+    });
+
     /**
      *  [2-2] After Rendering 함수 - 어떤 특정 상태(boxColor)의 변화에 반응(관심 분리)
      */
-    
-    
+    useEffect(() => {
+        console.log('After Rendering: boxColor Updated');
+    }, [boxColor]);
+
+    useEffect(() => {
+        console.log('After Rendering: title Updated');
+    }, [title]);
 
     /**
      *  [3] Alternative 03: componentDidMount & componentWillUnmount
      */
+    useEffect(() => {
+        console.log('After Mount(componentDidMount)');
 
+        return () => {
+            console.log('Before Unmount(componentWillUnmount)');
+        }
+    }, []);
 
 
 
--- 07.Component - Lifecycle_useEffect/ex04/src/App.js
+++ 07.Component - Lifecycle_useEffect/ex04/src/App.js
@@ -3,6 +3,30 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+
+    const [ticks, setTicks] = useState(0);
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    const getCurrentTime = () => {
+        const now = new Date();
+
+        return {
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds(),
+        }
+    };
+
+    useEffect(() => {
+        setInterval(() => {
+            setCurrentTime(getCurrentTime());
+        }, 1000);
+    }, []);
+
+    useEffect(() => {
+        setTicks(ticks+1);
+    }, [currentTime]);
+
     return (
         <Clock
             title={`ex04: Clock Component II: ${ticks}`}
