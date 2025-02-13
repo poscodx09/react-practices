29ffd323c86bd5ced7080ccab999eeb77ae1d9dc
ymj07168 <ymj07168@gmail.com>
2025-02-13 16:51:40 +0900

    open setOpen

--- 13.Integration/kanbanboard/frontend/src/Card.js
+++ 13.Integration/kanbanboard/frontend/src/Card.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {_Card, Card_Title, Card_Title_Open} from './assets/scss/Card.scss';
 import TaskList from './TaskList'
 
 export default function Card({no, title, description, status, tasks, handleAddTask, handleDeleteTask, handleCheckBox}) {
-  return (
-    <div className={_Card} key={no}>
-      <div className={[Card_Title, Card_Title_Open].join(' ')}>{title}</div>
+    const [open, setOpen] = useState(true);
+
+    return (
+    <div className={_Card} key={no} onClick={() => setOpen(!open)}>
+      <div className={[Card_Title, open ? Card_Title_Open : ''].join(' ')}>{title}</div>
+
       <div className='Card_Details'>
         {description}
       </div>
-      <TaskList no={no} tasks={tasks} handleAddTask={handleAddTask} handleDeleteTask={handleDeleteTask} handleCheckBox={handleCheckBox}/>
+      {
+        open && <TaskList no={no} tasks={tasks} handleAddTask={handleAddTask} handleDeleteTask={handleDeleteTask} handleCheckBox={handleCheckBox}/>
+      }
     </div>
   )
 }
