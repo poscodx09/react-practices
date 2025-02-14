6fe3e62159164540b0c3774d15b0678fd661b5f5
ymj07168 <ymj07168@gmail.com>
2025-02-14 13:41:54 +0900

    ex03

--- 08.Component - Immutability of State/ex03/src/App.js
+++ 08.Component - Immutability of State/ex03/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './assets/json/data.js';
+import update from 'react-addons-update';
+
+
+const GoodList = ({goods}) => {
+    return (
+        <ul>
+            {goods.map((g, _) => <li key={g}>{g.name}|{g.price}|{g.amount}</li>)}
+        </ul>
+    )
+}
 
 function App() {
+    const [order, setOrder] = useState(data);
 
     return (
         <div id='App'>
-            <p>{`배송지:`}</p>
-            <p>{`결제수단:`}</p>
+            <p>{`배송지:${order.receive}`}</p>
+            <p>{`결제수단:${order.payment.method}`}</p>
             <p>{'상품'}</p>
-            <button>
+            <GoodList goods={order.goods}/>
+            <button onClick={() => {
+                setOrder(
+                    update(order, {
+                        receive: {
+                            $set: "서우리 금천구 가산동"
+                       }
+                    })
+                )
+            }}>
                 {"배송지 수정"}
             </button>
         </div>
