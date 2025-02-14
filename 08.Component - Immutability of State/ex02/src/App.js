37e80e540143b0a7a92275a4e50d13d5e29dc186
ymj07168 <ymj07168@gmail.com>
2025-02-14 11:38:03 +0900

    react-addons-update

--- 08.Component - Immutability of State/ex02/src/App.js
+++ 08.Component - Immutability of State/ex02/src/App.js
@@ -1,35 +1,111 @@
-import React from 'react';
-import update from 'react-addons-update';import data from './assets/json/data.js';
+import React, { useState } from 'react';
+import update from 'react-addons-update';
+import data from './assets/json/data.js';
 
 function App() {
+    const [order, setOrder] = useState(data);
+
     return (
         <div id='App'>
-            <button>
+            <button onClick={() => {
+                // const orderUpdated = Object.assign({}, order);
+                // orderUpdated.receive = "서울시 금천구 가산동";
+
+                // sol.recommended: 프로퍼티
+                const orderUpdated = update(order, {
+                    receive: {
+                        $set: "서우리 금천구 가산동"
+                    }
+                });
+                setOrder(orderUpdated);
+            }}>
                 {"배송지 수정"}
             </button>
             <br/><br/>
 
-            <button>
+            <button onClick={() => {
+                // const orderUpdated = Object.assign({}, order);
+                // orderUpdated.payment = Object.assign({}, order.payment, {method: 'Cash'});
+                
+                // sol.recommended: nest 객체 프로퍼티 수정
+                const orderUpdated = update(order, {
+                    payment: {
+                        method: {
+                            $set: "Cash"
+                        }
+                    }
+                })
+                setOrder(orderUpdated);
+            }}>
                 {"결제수단 변경"}
             </button>
             <br/><br/>
+            <button onClick={() => {
+                // const orderUpdated = Object.assign({}, order);
+                // sol.1 
+                // const goodsUpdated = goods.concat(newObject);
 
-            <button>
+                // sol.2
+                // orderUpdated.goods = [...orderUpdated.goods, {
+                //     "no": "c002-003",
+                //     "name": "블루양말",
+                //     "price": 5000,
+                //     "amount": 3
+                // }]
+
+                // sol.recommended: nest 객체 프로퍼티 수정
+                const orderUpdated = update(order, {
+                    goods: {
+                        $unshift: [{
+                                "no": "c002-003",
+                                "name": "블루양말",
+                                "price": 5000,
+                                "amount": 3
+                        }]
+                    }
+                })
+                setOrder(orderUpdated);
+            }}>
                 {"상품 추가"}
             </button>
             <br/><br/>
 
-            <button>
+            <button onClick={() => {
+                // const orderUpdated = Object.assign({}, order);
+                // const goodsUpdate = [...orderUpdated.goods.slice(0,2),
+                //     Object.assign({}, orderUpdated.goods[2], {name:'블루면티'}),
+                //     ...orderUpdated.goods.slice(3)  
+                // ]
+                // orderUpdated.goods = goodsUpdate;
+                // setOrder(orderUpdated);
+
+                const orderUpdated = update(order, {
+                    goods: {
+                        2: {
+                            name: {
+                                $set: '블루면티'
+                            }
+                        }
+                    }
+                })
+                setOrder(orderUpdated);
+            }}>
                 {"3rd 상품이름 변경"}
             </button>
             <br/><br/>
 
             <hr/>
 
-            <p>{`배송지:`}</p>
-            <p>{`결제수단:`}</p>
+            <p>{`배송지: ${order.receive}`}</p>
+            <p>{`결제수단: ${order.payment.method}`}</p>
             <p>{'상품'}</p>
             <ul>
+                {
+                    order?.goods?.map((item) => 
+                    <li key={item.no}>
+                        상품명: {item.name} | 가격: {item.price.toLocaleString()}원 | 수량: {item.amount}
+                    </li>)
+                }
             </ul>
         </div>
     );
