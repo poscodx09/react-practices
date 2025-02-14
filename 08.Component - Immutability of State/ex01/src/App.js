c1aa9a7208a73df06b0846da36953d8ded300c61
ymj07168 <ymj07168@gmail.com>
2025-02-14 11:17:16 +0900

    ch08 ex01

--- 08.Component - Immutability of State/ex01/src/App.js
+++ 08.Component - Immutability of State/ex01/src/App.js
@@ -2,26 +2,54 @@ import React, { useState } from 'react';
 import data from './assets/json/data.js';
 
 function App() {
-    const [order, serOrder] = useState(data);
+    const [order, setOrder] = useState(data);
 
     return (
         <div id='App'>
-            <button>
+            <button onClick={() => {
+                const orderUpdated = Object.assign({}, order);
+                orderUpdated.receive = "서울시 금천구 가산동";
+                setOrder(orderUpdated);
+            }}>
                 {"배송지 수정"}
             </button>
             <br/><br/>
 
-            <button>
+            <button onClick={() => {
+                const orderUpdated = Object.assign({}, order);
+                orderUpdated.payment = Object.assign({}, order.payment, {method: 'Cash'});
+                setOrder(orderUpdated);
+            }}>
                 {"결제수단 변경"}
             </button>
             <br/><br/>
+            <button onClick={() => {
+                const orderUpdated = Object.assign({}, order);
+                // sol.1 
+                // const goodsUpdated = goods.concat(newObject);
 
-            <button>
+                // sol.2
+                orderUpdated.goods = [...orderUpdated.goods, {
+                    "no": "c002-003",
+                    "name": "블루양말",
+                    "price": 5000,
+                    "amount": 3
+                }]
+                setOrder(orderUpdated);
+            }}>
                 {"상품 추가"}
             </button>
             <br/><br/>
 
-            <button>
+            <button onClick={() => {
+                const orderUpdated = Object.assign({}, order);
+                const goodsUpdate = [...orderUpdated.goods.slice(0,2),
+                    Object.assign({}, orderUpdated.goods[2], {name:'블루면티'}),
+                    ...orderUpdated.goods.slice(3)  
+                ]
+                orderUpdated.goods = goodsUpdate;
+                setOrder(orderUpdated);
+            }}>
                 {"3rd 상품이름 변경"}
             </button>
             <br/><br/>
