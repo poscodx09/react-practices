626545792b13a4fa1c197e37b600b18f325fe6ca
ymj07168 <ymj07168@gmail.com>
2025-02-14 15:21:29 +0900

    config

--- 13.Integration/tabbox/backend/src/main/java/com/poscodx/tabbox/controller/WhitelabelErrorController.java
+++ 13.Integration/tabbox/backend/src/main/java/com/poscodx/tabbox/controller/WhitelabelErrorController.java
@@ -1,22 +1,23 @@
 package com.poscodx.tabbox.controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/error")
 public class WhitelabelErrorController implements ErrorController {
-	
+
 	@RequestMapping("/404")
 	public String _404() {
 		return "errors/404";
 	}
-	
+
 	@RequestMapping("/500")
 	public String _500() {
 		return "errors/500";
@@ -24,23 +25,23 @@ public class WhitelabelErrorController implements ErrorController {
 
 	@RequestMapping("")
 	public String handleError(HttpServletRequest request) {
-	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-	    
-	    if(status != null) {
-	        Integer statusCode = Integer.valueOf(status.toString());
-	    
-	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return "errors/404";
-	        } else if(statusCode == HttpStatus.BAD_REQUEST.value()) {
-	            return "errors/400";
-	        } else if(statusCode == HttpStatus.FORBIDDEN.value()) {
-	            return "errors/403";
-	        } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return "errors/500";
-	        } 
-	    }
-	    
-	    return "errors/Unknown";
-	}	
-	
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "errors/404";
+			} else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+				return "errors/400";
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				return "errors/403";
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				return "errors/500";
+			}
+		}
+
+		return "errors/Unknown";
+	}
+
 }
